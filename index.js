a0f18baa4dc7085455957d7784d761be967f55e9
vishalini <vishalini.r@skava.com>
2018-06-12 19:11:31 +0530

    'seperted-code'

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
-const app = {
-    name: 'demo',
+// const app = {
+//     name: 'demo',
+// }
+
+// exports.app = app
+
+const express = require('express')
+
+const app = express()
+
+function runOne() {
+    app.get('/demo',(req,res) => 
+    res
+    .status(200)
+    .json({name:"vishalini"})
+)
+app.use((req,res,next) => {
+    console.log('[server] hit *')
+    req.eh = 1
+    next()
+})
+app.get('*',(req,res) => 
+    console.log('[server] hit *', req.eh)
+)
+
+const onListen = () =>
+{
+    console.log('http://localhost:5888')
 }
 
-exports.app = app
+app.listen(5888,onListen)
+
+}
+
+function runTwo() {
+    app.get('/demo',(req,res) => 
+    res
+    .status(200)
+    .json({name:"kiruthika"})
+)
+app.use((req,res,next) => {
+    console.log('[server] hit *')
+    req.eh = 1
+    next()
+})
+app.get('*',(req,res) => 
+    console.log('[server] hit *', req.eh)
+)
+
+const onListen = () =>
+{
+    console.log('http://localhost:4444')
+}
+
+app.listen(4444,onListen)
+
+}
+
+runTwo()
+runOne()
+
